df7afe724f020af73bbb17f476d453cf2a7720bd
Sean Brock <sdbrock@proton.me>
2025-04-13 14:36:13 -0500

    Modify hello.rs to take input name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use std::io::stdin;
+
+fn get_name() -> String {
+    let mut your_name = String::new();
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+    your_name.trim().to_lowercase()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, {:?}", get_name());
 }
